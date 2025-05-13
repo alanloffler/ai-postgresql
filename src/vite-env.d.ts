65cb32b1d00e5417236259ffde36f66aa2ae770e
alanloffler <alanmatiasloffler@gmail.com>
2025-05-13 18:52:08 -0300

    Add supabase env variables
    For PostgreSQL database

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+interface ImportMetaEnv {
+  readonly VITE_POSTGRES_CONN: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly VITE_SUPABASE_URL: string;
+}
