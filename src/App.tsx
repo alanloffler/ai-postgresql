b439fc26f05cd85594869eb67572d4caf2633719
alanloffler <alanmatiasloffler@gmail.com>
2025-05-14 22:07:38 -0300

    Implement search and datatable component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card";
-import { Input } from "@components/ui/input";
-import { useState } from "react";
-import type { IProfessional } from "./interfaces/professional.interface";
+import { DataTable } from "@components/DataTable";
+import { Search } from "@components/Search";
+import { Card, CardContent, CardHeader, CardTitle } from "@components/ui/card";
 import { createClient } from "@supabase/supabase-js";
+import { useState } from "react";
 import { generateQuery } from "./actions/ai";
+import type { IResult } from "./types/result.type.ts";
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
@@ -13,55 +13,42 @@ const supabase = createClient(
 
 function App() {
   const [inputValue, setInputValue] = useState<string>("");
-  const [professionals, setProfessionals] = useState<IProfessional[] | null>(
-    null,
-  );
+  const [sqlData, setSqlData] = useState<IResult | null>(null);
 
-  async function getProfessionals(query: string): Promise<void> {
+  async function getProfessionals(query?: string): Promise<void> {
     const { data, error } = await supabase.rpc("execute_sql", {
       query_text: query,
+      // params: [],
     });
 
     if (error) console.log(error);
     console.log(data);
-    setProfessionals(data);
+
+    setSqlData(data);
   }
 
   async function handleSubmit() {
-    console.log(inputValue);
     const query = await generateQuery(inputValue);
     console.log(query);
     await getProfessionals(query.slice(0, -1));
+    // TODO: handle error
   }
 
   return (
     <main className="flex h-full p-8 items-center justify-center">
-      <Card className="w-2xl">
+      <Card className="w-full max-w-2xl">
         <CardHeader>
           <CardTitle className="font-bold text-2xl">
             Consulta la Base de Datos
           </CardTitle>
         </CardHeader>
-        <CardContent>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            <section className="flex gap-6">
-              <Input
-                type="text"
-                placeholder="Ingresa tu consulta"
-                value={inputValue}
-                onChange={(e) => setInputValue(e.target.value)}
-              />
-              <Button type="submit" variant="default">
-                Buscar
-              </Button>
-            </section>
-          </form>
-          <section>{JSON.stringify(professionals)}</section>
+        <CardContent className="space-y-8">
+          <Search
+            input={inputValue}
+            setInput={setInputValue}
+            submit={handleSubmit}
+          />
+          <DataTable data={sqlData} />
         </CardContent>
       </Card>
     </main>
